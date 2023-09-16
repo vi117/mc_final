3a338271984af891b264c409a7a674a1e29bcb3e
monoid <qaswedfr55@gmail.com>
2023-09-16 15:34:53 +0900

    feat: register articleRouter

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,3 +1,4 @@
+import articleRouter from "@/article/router";
 import fundingRouter from "@/fundings/router";
 import { tokenCheckMiddleware } from "@/users/jwt";
 import userRouter from "@/users/router";
@@ -8,6 +9,7 @@ const router = Router();
 router.use("/", tokenCheckMiddleware);
 router.use("/api/users", userRouter);
 router.use("/api/fundings", fundingRouter);
+router.use("/api/articles", articleRouter);
 
 router.get("/", (_req, res) => {
   res.json({ message: "hello" });
