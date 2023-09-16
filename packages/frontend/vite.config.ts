8d7b9e06d9cf8dfd2e38a05bd0676bf4337381fc
monoid <qaswedfr55@gmail.com>
2023-09-16 11:08:43 +0900

    chore: set proxy for api server

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,4 +5,15 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, "/api"),
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
