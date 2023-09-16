2ea56f98cdf6e81f08b679ccd0c645f9a0ff41fb
godus2793 <godus@naver.com>
2023-09-16 16:32:57 +0900

    feat: 회원 탈퇴 페이지 수정

--- packages/frontend/src/pages/withdrawal/index.jsx
+++ packages/frontend/src/pages/withdrawal/index.jsx
@@ -1,84 +1,29 @@
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { Alert } from "react-bootstrap";
+import images from "./images/logo.svg";
 import classes from "./withdrawal.module.css";
-
-import { styled } from "@mui/material";
-import { Alert, Button, Form } from "react-bootstrap";
-
-export function BasicExample() {
-  return (
-    <>
-      {[
-        "danger",
-      ].map((variant) => (
-        <Alert key={variant} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
-    </>
-  );
-}
-
-export function FormTextExample() {
-  return (
-    <>
-      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-      <Form.Control
-        type="password"
-        id="inputPassword5"
-        aria-describedby="passwordHelpBlock"
-      />
-      <Form.Text id="passwordHelpBlock" muted>
-        Your password must be 8-20 characters long, contain letters and numbers,
-        and must not contain spaces, special characters, or emoji.
-      </Form.Text>
-    </>
-  );
-}
-
-export function CheckExample() {
-  return (
-    <Form>
-      {["checkbox"].map((type) => (
-        <div key={`default-${type}`} className="mb-3">
-          <Form.Check // prettier-ignore
-            type={type}
-            id={`default-${type}`}
-            label={`default ${type}`}
-          />
-          탈퇴 후 복구 불가함을 확인하였습니다.
-        </div>
-      ))}
-    </Form>
-  );
-}
-
-export function TypesExample() {
-  return (
-    <>
-      <Button variant="secondary">Secondary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>
-      {" "}
-    </>
-  );
-}
-
-const Foo = styled("Foo")({
-  background: "gray",
-  width: "50px",
-  height: "50px",
-});
 
 export default function WithdrawalPage() {
   return (
     <div className={classes["container"]}>
-      <h2>회원탈퇴</h2>
+      <h2 style={{ color: "#1877F2" }}>회원 탈퇴</h2>
       <Alert variant="danger">
         탈퇴 시 전자금융거래법에 따라 금융 거래기록은 5년 뒤 파기됩니다. 다른
         모든 정보는 삭제되며 복구는 불가합니다.
       </Alert>
-      <Button>cbc</Button>
-      <Button>abc</Button>
-      <Foo></Foo>
-      <div></div>
+      <div className={["img-wrapper"]}>
+        <img src={images} alt="logo" />
+      </div>
+      <h4>HappyTails 탈퇴전 꼭 확인하세요</h4>
+      <p>탈퇴하시면 이용중인 모든 페이지가 폐쇄되며,</p>
+      <p>모든 데이터는 복구가 불가능합니다.</p>
+
+      <div className={classes["CheckboxLabels"]}>
+        <FormControlLabel
+          control={<Checkbox defaultChecked />}
+          label="안내사항을 모두 확인하였으며, 이에 동의합니다."
+        />
+      </div>
     </div>
   );
 }
