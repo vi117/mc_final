a776b33c2a56dd262b240cb22fe8bfd606fa204a
monoid <qaswedfr55@gmail.com>
2023-09-17 22:16:24 +0900

    feat: change verification code to jwt

--- packages/backend/src/users/authCodeRepo.ts
+++ packages/backend/src/users/authCodeRepo.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "crypto";
+import jwt from "jsonwebtoken";
 
 type VerifyCodeType = string;
 
@@ -6,34 +6,18 @@ type VerifyCodeType = string;
  * 사용자가 생성될 때 사용자가 이메일을 가지고 있는지 확인하는 저장소입니다.
  */
 export class AuthCodeRepository {
-  private userMap: Map<VerifyCodeType, string>;
-  private dateMap: Map<number, VerifyCodeType>;
-
   constructor() {
-    this.userMap = new Map();
-    this.dateMap = new Map();
-  }
-
-  cleanExpired() {
-    const age = 5 * 60 * 1000;
-    for (const time of this.dateMap.keys()) {
-      if (time + age < Date.now()) {
-        const key = this.dateMap.get(time);
-        this.dateMap.delete(time);
-        if (key === undefined) {
-          // this should not happen
-          throw new Error("unreachable!");
-        }
-        this.userMap.delete(key);
-      }
-    }
   }
 
   createVerificationCode(email: string) {
-    const key = randomUUID();
-    this.userMap.set(key, email);
-    this.dateMap.set(Date.now(), key);
-    return key;
+    const content = {
+      email: email,
+    };
+    const secret = (process.env.JWT_SECRET as string) + "verificationCode";
+    const token = jwt.sign(content, secret, {
+      expiresIn: "5m",
+    });
+    return token;
   }
 
   /**
@@ -43,12 +27,20 @@ export class AuthCodeRepository {
    * @return {string | null} - The email address associated with the code, or null if the code is invalid.
    */
   verify(code: VerifyCodeType): string | null {
-    this.cleanExpired();
-    const email = this.userMap.get(code);
-    if (email === undefined) {
-      return null;
+    try {
+      const payload = jwt.verify(code, process.env.JWT_SECRET as string);
+      const content = payload as { email: string };
+      const email = content.email;
+      if (email === undefined) {
+        return null;
+      }
+      return email;
+    } catch (e) {
+      if (e instanceof jwt.JsonWebTokenError) {
+        return null;
+      }
+      throw e;
     }
-    return email;
   }
 }
 
