47a6af4a96ba2fb747b3b373b29a16ff8389de99
godus2793 <godus@naver.com>
2023-09-18 17:45:52 +0900

    fix: reset-password 페이지 수정 완료

--- packages/frontend/src/pages/reset-password/index.jsx
+++ packages/frontend/src/pages/reset-password/index.jsx
@@ -1,10 +1,14 @@
-import { FloatingLabel, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form } from "react-bootstrap";
 import classes from "./reset-password.module.css";
 
 export default function ResetpasswordPage() {
   return (
     <div className={classes["container"]}>
       <h3>비밀번호 재설정</h3>
+      <p>
+        가입하신 이메일 주소를 입력하시면 <br></br>
+        <strong>'비밀번호 재설정'</strong> 안내 메일이 전송됩니다.
+      </p>
       <div className={classes["FloatingLabel"]}>
         <FloatingLabel
           controlId="floatingInput"
@@ -13,6 +17,11 @@ export default function ResetpasswordPage() {
         >
           <Form.Control type="email" placeholder="name@example.com" />
         </FloatingLabel>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="lg">
+            전송
+          </Button>
+        </div>
       </div>
     </div>
   );
