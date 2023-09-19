9f709fb567a1173a9cff6d785bd5d9ab419a1fdb
DESKTOP-H0VLM65\seojun <cjsdnjsrl96@naver.com>
2023-09-19 10:49:59 +0900

    fix: 약관코드 수정, index내용 수정

--- packages/frontend/src/pages/register/registerArgeeModal.jsx
+++ packages/frontend/src/pages/register/registerArgeeModal.jsx
@@ -1,41 +1,35 @@
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Index from "./registerArgee/index";
+import TermOfPolicy from "./TermOfPolicy";
 
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+function RegisterArgee(props) {
+  const {
+    show,
+    handleClose,
+    handleConfirm,
+    ...rest
+  } = props;
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        HappyTails 회원가입 약관
-      </Button>
-
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>회원가입 약관</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Index></Index>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            취소
-          </Button>
-          <Button variant="primary">확인</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
+    <Modal
+      show={show}
+      onHide={handleClose}
+      {...rest}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>회원가입 약관</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <TermOfPolicy></TermOfPolicy>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          취소
+        </Button>
+        <Button variant="primary" onClick={handleConfirm}>확인</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
-export default Example;
+export default RegisterArgee;
