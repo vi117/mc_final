0290c82ebef382c9a9013134fc20a249dc2b0cd6
godus2793 <godus@naver.com>
2023-09-22 14:13:27 +0900

    Co-authored-by: vi117 <vi117@users.noreply.github.com> Co-authored-by: DANG-HJ <DANG-HJ@users.noreply.github.com>

--- packages/frontend/src/pages/fundings/FundingsPay.jsx
+++ packages/frontend/src/pages/fundings/FundingsPay.jsx
@@ -5,7 +5,12 @@ import classes from "./FundingsPay.module.css";
 
 export default function FundingsPay() {
   const location = useLocation();
-  const { funding, selectedReward } = location.state;
+  const state = location.state;
+  if (!state) {
+    return <div>Funding에서 클릭해야 합니다!</div>;
+  }
+
+  const { funding, selectedReward } = state;
 
   const remainingDays =
     (new Date(funding.end_date).getTime() - new Date().getTime())
@@ -120,7 +125,7 @@ function FundingPrecaution() {
         <Accordion.Header>후원 유의사항</Accordion.Header>
         <Accordion.Body>
           후원은 구매가 아닌 창의적인 계획에 자금을 지원하는 일입니다.
-          텀블벅에서의 후원은 아직 실현되지 않은 프로젝트가 실현될 수 있도록
+          HappyTails에서의 후원은 아직 실현되지 않은 프로젝트가 실현될 수 있도록
           제작비를 후원하는 과정으로, 기존의 상품 또는 용역을 거래의 대상으로
           하는 매매와는 차이가 있습니다. 따라서 전자상거래법상 청약철회 등의
           규정이 적용되지 않습니다.
