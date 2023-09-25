5c0b837099585c62c8ddf726a185cb77cb2d9515
monoid <qaswedfr55@gmail.com>
2023-09-25 13:12:32 +0900

    fix: profile css rework

---
+++ packages/frontend/src/pages/profile/profilePage.tsx
@@ -0,0 +1,41 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useLoginInfo } from "../../hook/useLogin";
+import AccordionList from "./profileLikeList";
+
+export default function Profile() {
+  const userInfo = useLoginInfo();
+
+  if (!userInfo) {
+    // Todo: 로그인이 안된경우 fallback
+    return <div>로그인이 필요합니다.</div>;
+  }
+  // Todo:
+  //   api 연결 후 닉네임 집어넣기
+  //   내가 만든 펀딩 및 후원 펀딩 리스트 연결하기
+  return (
+    <Container className="my-5">
+      <Row>
+        <Col md={3} className="my-3">
+          <Row>
+            <span>{userInfo.nickname}</span>
+          </Row>
+          <hr></hr>
+          <Row>
+            <Link to={`/profile/edit`}>
+              내정보 수정
+            </Link>
+          </Row>
+          <Row>
+            <Link to={`/withdraw`}>
+              회원탈퇴
+            </Link>
+          </Row>
+        </Col>
+        <Col md={9}>
+          <AccordionList />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
