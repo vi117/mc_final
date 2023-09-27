8da7b4a7ed602806fea1b47834f34bd2b0f2864d
monoid <qaswedfr55@gmail.com>
2023-09-27 14:16:35 +0900

    refactor: extract setIntereset to api

--- packages/frontend/src/api/funding.ts
+++ packages/frontend/src/api/funding.ts
@@ -16,3 +16,26 @@ export async function withdrawFunding(id: number, selectedReward_id: number) {
     throw new APIError(data.message);
   }
 }
+
+export async function setFundingInterest(id: number, like = true) {
+  const url = new URL(
+    `/api/v1/fundings/${id}/interest`,
+    window.location.origin,
+  );
+  url.searchParams.append("disset", (!like) ? "true" : "false");
+  const res = await fetch(url.href, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (res.status === 401) {
+    // need login
+    return "Unauthorized";
+  } else if (res.status === 409) {
+    // already set
+    return "Conflict";
+  } else {
+    return "OK";
+  }
+}
