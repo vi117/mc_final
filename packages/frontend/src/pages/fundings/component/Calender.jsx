27f861090e654487f412066905f3e5b4de4b7f74
DESKTOP-0L5RNVV\윤희 <jaddong66@gmail.com>
2023-09-27 14:27:21 +0900

    feat: fix funding write css (responsive)

--- packages/frontend/src/pages/fundings/component/Calender.jsx
+++ packages/frontend/src/pages/fundings/component/Calender.jsx
@@ -10,37 +10,41 @@ export const Calender = ({
 }) => {
   return (
     <div className={classes.date_area}>
-      <span>시작일</span>
-      <DatePicker
-        className={classes.date_picker}
-        selected={startDate}
-        onChange={(date) =>
-          onChange({
-            startDate: date,
-            endDate,
-          })}
-        selectsStart
-        startDate={startDate}
-        endDate={endDate}
-        locale={ko}
-        dateFormat="yyyy년 MM월 dd일"
-      />
-      <span>종료일</span>
-      <DatePicker
-        className={classes.date_picker}
-        selected={endDate}
-        onChange={(date) =>
-          onChange({
-            endDate: date,
-            startDate,
-          })}
-        selectsEnd
-        startDate={startDate}
-        endDate={endDate}
-        minDate={startDate}
-        locale={ko}
-        dateFormat="yyyy년 MM월 dd일"
-      />
+      <div className={classes.date_item}>
+        <span>시작일</span>
+        <DatePicker
+          className={classes.date_picker}
+          selected={startDate}
+          onChange={(date) =>
+            onChange({
+              startDate: date,
+              endDate,
+            })}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+          locale={ko}
+          dateFormat="yyyy년 MM월 dd일"
+        />
+      </div>
+      <div className={classes.date_item}>
+        <span>종료일</span>
+        <DatePicker
+          className={classes.date_picker}
+          selected={endDate}
+          onChange={(date) =>
+            onChange({
+              endDate: date,
+              startDate,
+            })}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+          locale={ko}
+          dateFormat="yyyy년 MM월 dd일"
+        />
+      </div>
     </div>
   );
 };
