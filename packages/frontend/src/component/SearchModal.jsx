bfbc200b98ef9b675a26325259b5cf35fac6c5ed
DHJ <dhj0422@naver.com>
2023-10-04 12:58:14 +0900

    refactor : searchbar 펀딩 제목 검색(태그 검색은 안됨)

--- packages/frontend/src/component/SearchModal.jsx
+++ packages/frontend/src/component/SearchModal.jsx
@@ -4,14 +4,24 @@ import { NavLink } from "react-router-dom";
 import classes from "./SearchModal.module.css";
 import SearchModalBasic from "./SearchModalBasic";
 
-// 검색 모달을 띄우는 페이지
 function SearchModal() {
-  // 모달창 노출 여부 state
   const [modalOpen, setModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  // 모달창 노출
   const openModal = () => {
     setModalOpen(true);
+  };
+
+  const performSearch = () => {
+    if (searchTerm) {
+      window.location.href = `/fundings?title=${searchTerm}`;
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      performSearch();
+    }
   };
 
   return (
@@ -19,11 +29,15 @@ function SearchModal() {
       <div className={classes["search_bar_container"]}>
         <input
           className={classes["search_bar"]}
-          // type="text"
           placeholder="검색어를 입력하세요."
-          onClick={openModal} // 검색 창을 클릭하면 모달 열기
+          onClick={openModal}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          onKeyDown={handleKeyDown}
         />
-        <NavLink to={"/fundings"}>
+
+        <NavLink to="#" onClick={performSearch}>
           <FiSearch className={classes["search_bar_button"]} />
         </NavLink>
       </div>
--- packages/frontend/src/component/SearchModalBasic.tsx
+++ packages/frontend/src/component/SearchModalBasic.tsx
@@ -2,11 +2,6 @@ import { useEffect, useRef } from "react";
 import { IoMdClose } from "react-icons/io";
 import styles from "./SearchModalBasic.module.css";
 
-// PropsType 정의/////// 이코드가 없어도 작동하는데 오류가뜨네요..
-type PropsType = {
-  setModalOpen: (open: boolean) => void;
-};
-
 function SearchModalBasic({ setModalOpen }: PropsType) {
   // 모달 끄기
   const closeModal = () => {
@@ -62,67 +57,3 @@ function SearchModalBasic({ setModalOpen }: PropsType) {
   );
 }
 export default SearchModalBasic;
-
-// import { useEffect, useRef } from "react";
-// import { IoMdClose } from "react-icons/io";
-// import styles from "./SearchModalBasic.module.css";
-
-// // PropsType 정의/////// 이코드가 없어도 작동하는데 오류가뜨네요..
-// type PropsType = {
-//   setModalOpen: (open: boolean) => void;
-// };
-
-// function SearchModalBasic({ setModalOpen }: PropsType) {
-//   // 모달 끄기
-//   const closeModal = () => {
-//     setModalOpen(false);
-//   };
-
-//   // 모달 외부 클릭시 끄기 처리
-//   // Modal 창을 useRef로 취득
-//   const modalRef = useRef<HTMLDivElement>(null);
-
-//   useEffect(() => {
-//     // 이벤트 핸들러 함수
-//     const handler = () => { // event 매개변수를 추가
-//       // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
-//       if (modalRef.current && !modalRef.current.contains(event.target)) {
-//         setModalOpen(false);
-//       }
-//     };
-
-//     // 이벤트 핸들러 등록
-//     document.addEventListener("mousedown", handler);
-
-//     return () => {
-//       // 이벤트 핸들러 해제
-//       document.removeEventListener("mousedown", handler);
-//     };
-//   });
-
-//   return (
-//     <div ref={modalRef} className={styles.container}>
-//       <button className={styles.close} onClick={closeModal}>
-//         <IoMdClose />
-//       </button>
-//       <div>
-//         <p>추천 검색어:</p>
-//         <ul>
-//           <li>검색어1</li>
-//           <li>검색어2</li>
-//           <li>검색어3</li>
-//         </ul>
-//       </div>
-
-//       <div>
-//         <p>연관 검색어:</p>
-//         <ul>
-//           <li>연관어1</li>
-//           <li>연관어2</li>
-//           <li>연관어3</li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-// export default SearchModalBasic;
