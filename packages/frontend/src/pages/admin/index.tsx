5955a2861c36d9799d112b12526477eca76840ee
DESKTOP-0L5RNVV\윤희 <jaddong66@gmail.com>
2023-10-11 09:13:37 +0900

    feat: add morebtn in funding/community report

--- packages/frontend/src/pages/admin/index.tsx
+++ packages/frontend/src/pages/admin/index.tsx
@@ -47,73 +47,77 @@ function FundingRequestManageTap() {
               )}
               </div> */
               }
-              <div>
+              <div className={classes["fundingState_img"]}>
                 <Link to={`/fundings/request/${funding.id}`}>
                   <img src={funding.thumbnail}></img>
                 </Link>
               </div>
-              <div className={classes["fundingState_textarea"]}>
-                <div
-                  className={classes[
-                    funding.funding_state === 2
-                      ? "rejected"
-                      : "fundingState_value"
-                  ]}
-                >
-                  {getStateTextFromFundingState(funding.funding_state)}
-                </div>
-                <Link to={`/fundings/request/${funding.id}`}>
-                  <div className={classes["FundingTitle"]}>
-                    {funding.meta_parsed?.tags
-                      && funding.meta_parsed.tags.some(tag => tag.trim() !== "")
-                      && (
-                        funding.meta_parsed.tags.map((tag) => (
-                          <div className={classes["FundingTag"]} key={tag}>
-                            [{tag}]
-                          </div>
-                        ))
-                      )}
-                    {funding.title}
+              <div className={classes["fundingState_text_wrap"]}>
+                <div className={classes["fundingState_textarea"]}>
+                  <div
+                    className={classes[
+                      funding.funding_state === 2
+                        ? "rejected"
+                        : "fundingState_value"
+                    ]}
+                  >
+                    {getStateTextFromFundingState(funding.funding_state)}
                   </div>
-                </Link>
-                <table className={classes["funding_profile_table"]}>
-                  <tbody>
-                    <tr>
-                      <td>호스트</td>
-                      <td>{funding.host_nickname} ({funding.host_email})</td>
-                    </tr>
-                    <tr>
-                      <td>목표</td>
-                      <td>{funding.target_value}원</td>
-                    </tr>
-                    <tr>
-                      <td>펀딩기간</td>
-                      <td>
-                        {formatDate(funding.begin_date)} ~{" "}
-                        {formatDate(funding.end_date)}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>신청일</td>
-                      <td>{formatDate(funding.created_at)}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className={classes["button_area"]}>
-                <button
-                  className={classes["button_approve"]}
-                  onClick={() => onApproveClick(funding.id)}
-                >
-                  승인
-                </button>
-                <button
-                  color="error"
-                  style={{ left: "5px" }}
-                  onClick={() => onRejectClick(funding.id)}
-                >
-                  거부
-                </button>
+                  <Link to={`/fundings/request/${funding.id}`}>
+                    <div className={classes["FundingTitle"]}>
+                      {funding.meta_parsed?.tags
+                        && funding.meta_parsed.tags.some(tag =>
+                          tag.trim() !== ""
+                        )
+                        && (
+                          funding.meta_parsed.tags.map((tag) => (
+                            <div className={classes["FundingTag"]} key={tag}>
+                              [{tag}]
+                            </div>
+                          ))
+                        )}
+                      {funding.title}
+                    </div>
+                  </Link>
+                  <table className={classes["funding_profile_table"]}>
+                    <tbody>
+                      <tr>
+                        <td>호스트</td>
+                        <td>{funding.host_nickname} ({funding.host_email})</td>
+                      </tr>
+                      <tr>
+                        <td>목표</td>
+                        <td>{funding.target_value}원</td>
+                      </tr>
+                      <tr>
+                        <td>펀딩기간</td>
+                        <td>
+                          {formatDate(funding.begin_date)} ~{" "}
+                          {formatDate(funding.end_date)}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>신청일</td>
+                        <td>{formatDate(funding.created_at)}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div className={classes["button_area"]}>
+                  <button
+                    className={classes["button_approve"]}
+                    onClick={() => onApproveClick(funding.id)}
+                  >
+                    승인
+                  </button>
+                  <button
+                    color="error"
+                    style={{ left: "5px" }}
+                    onClick={() => onRejectClick(funding.id)}
+                  >
+                    거부
+                  </button>
+                </div>
               </div>
             </div>
           );
@@ -162,7 +166,12 @@ function FundingRequestManageTap() {
   }
 }
 
-function FundingReportManageTap() {
+function FundingReportManageTap(
+  { expandedContent, toggleExpand }: {
+    expandedContent: Record<number, boolean>;
+    toggleExpand: (id: number) => void;
+  },
+) {
   const { data, error, isLoading } = useFundingReports({
     limit: 50,
     offset: 0,
@@ -191,13 +200,29 @@ function FundingReportManageTap() {
           return (
             <tbody key={x.id}>
               <tr>
-                <td>{x.id}</td>
+                <td className={classes["number"]}>1{x.id}</td>
                 <td>
                   <Link to={`/fundings/${x.funding_id}`}>
                     {x.funding_title}
                   </Link>
                 </td>
-                <td className={classes["reason_content"]}>{x.content}</td>
+                <td className={classes["reason_content"]}>
+                  {expandedContent[x.id]
+                    ? x.content
+                    : x.content.substring(0, 110)}
+                  {x.content.length > 110 && (
+                    <button
+                      className={classes["moreButton"]}
+                      onClick={() => toggleExpand(x.id)}
+                    >
+                      {expandedContent[x.id] ? "[접기]" : (
+                        <span>
+                          <b>···</b>더보기
+                        </span>
+                      )}
+                    </button>
+                  )}
+                </td>
               </tr>
             </tbody>
           );
@@ -207,7 +232,12 @@ function FundingReportManageTap() {
   );
 }
 
-function CommunityReport() {
+function CommunityReport(
+  { expandedContent, toggleExpand }: {
+    expandedContent: Record<number, boolean>;
+    toggleExpand: (id: number) => void;
+  },
+) {
   const { data, error, isLoading } = useArticleReports({
     limit: 50,
     offset: 0,
@@ -219,6 +249,7 @@ function CommunityReport() {
   if (error) {
     return <div>에러가 발생했습니다.</div>;
   }
+
   return (
     <div className={classes["admin_contents_container"]}>
       <h3>커뮤니티 신고 관리</h3>
@@ -241,7 +272,23 @@ function CommunityReport() {
                     {c.article_title}
                   </Link>
                 </td>
-                <td>{c.content}</td>
+                <td className={classes["reason_content"]}>
+                  {expandedContent[c.id]
+                    ? c.content
+                    : c.content.substring(0, 110)}
+                  {c.content.length > 110 && (
+                    <button
+                      className={classes["moreButton"]}
+                      onClick={() => toggleExpand(c.id)}
+                    >
+                      {expandedContent[c.id] ? "[접기]" : (
+                        <span>
+                          <b>···</b>더보기
+                        </span>
+                      )}
+                    </button>
+                  )}
+                </td>
               </tr>
             </tbody>
           );
@@ -253,6 +300,17 @@ function CommunityReport() {
 
 export default function AdminPage() {
   const [key, setKey] = useState("request");
+  const [expandedContent, setExpandedContent] = useState<
+    Record<number, boolean>
+  >({});
+
+  const toggleExpand = (id: number) => {
+    setExpandedContent((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  };
+
   return (
     <div className={classes["admin_container"]}>
       <Tabs
@@ -265,10 +323,16 @@ export default function AdminPage() {
           <FundingRequestManageTap />
         </Tab>
         <Tab eventKey="report" title="펀딩 신고 관리">
-          <FundingReportManageTap />
+          <FundingReportManageTap
+            expandedContent={expandedContent}
+            toggleExpand={toggleExpand}
+          />
         </Tab>
         <Tab eventKey="co_report" title="커뮤니티 신고 관리">
-          <CommunityReport />
+          <CommunityReport
+            expandedContent={expandedContent}
+            toggleExpand={toggleExpand}
+          />
         </Tab>
       </Tabs>
     </div>
