25666c2f5d5c109665051e8561fa4cde96f0d5ea
monoid <qaswedfr55@gmail.com>
2023-10-12 20:26:03 +0900

    fix: strict off when production

--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+if (import.meta.env.DEV) {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+} else {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App />,
+  );
+}
